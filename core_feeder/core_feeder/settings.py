6dcf81c6079c54845535a3535c4641a23f9b6cb5
Subarna Sahoo <48875593+subarna-sahoo@users.noreply.github.com>
2021-04-07 16:19:41 +0530

    Update settings.py

--- core_feeder/core_feeder/settings.py
+++ core_feeder/core_feeder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1r8@jz(!0h=+p-ev!tn#9!*#eerayx8!fa@jzgd%gbp=nszsel
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
